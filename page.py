eebe1ba5436e60693c2ec88bc0d363cd1f71d4b1
Hiroto Nitta <hnitta@Hirotos-MacBook-Air.local>
2026-05-24 21:14:44 +0900

    Rename and touch

---
+++ page.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+pg = st.navigation([
+    st.Page("TopPage.py", title="00 トップページ"),
+    st.Page("pages/Palabola.py", title="01 放物運動"),
+    st.Page("pages/CircleMotion.py", title="02 等速円運動"),
+    ])
+
+pg.run()
