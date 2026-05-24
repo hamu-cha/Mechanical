3ad957d8536e8e3661c5439622de04edc11b3cfa
Hiroto Nitta <hnitta@Hirotos-MacBook-Air.local>
2026-05-24 21:22:04 +0900

    modify

--- page.py
+++ page.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 pg = st.navigation([
-    st.Page("TopPage.py", title="00 トップページ"),
+    st.Page("pages/TopPage.py", title="00 トップページ"),
     st.Page("pages/Palabola.py", title="01 放物運動"),
     st.Page("pages/CircleMotion.py", title="02 等速円運動"),
     ])
